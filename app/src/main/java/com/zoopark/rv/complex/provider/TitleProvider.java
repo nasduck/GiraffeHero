0bdcfc0847838044d7d9fa80b8ef87a1b5fa95ff
Chuan Dong <dongchuanyz@163.com>
2019-05-30 16:02:15 +0800

    Add title provider and read more provider

---
+++ app/src/main/java/com/zoopark/rv/complex/provider/TitleProvider.java
@@ -0,0 +1,29 @@
+package com.zoopark.rv.complex.provider;
+
+import android.content.Context;
+
+import com.nasduck.rvprovider.R;
+import com.zoopark.rv.BaseItemProvider;
+import com.zoopark.rv.BaseViewHolder;
+import com.zoopark.rv.IndexPath;
+
+public class TitleProvider extends BaseItemProvider<Object, BaseViewHolder> {
+
+    private String mTitle;
+
+    public TitleProvider(Context context, String title) {
+        super(context);
+
+        this.mTitle = title;
+    }
+
+    @Override
+    public int getLayout() {
+        return R.layout.item_common_title;
+    }
+
+    @Override
+    public void onBind(BaseViewHolder holder, IndexPath indexPath) {
+        holder.setText(R.id.tv_left_title, mTitle);
+    }
+}
