0df2b7958749ae5145c675ac8280cd55d72856a7
redrain <m13646668116_1@163.com>
2019-10-16 11:03:03 +0800

    Fixed #10 Add change animation and add more configs about item animator

--- lib/src/main/java/com/zoopark/rv/animation/FlipItemAnimator.java
+++ lib/src/main/java/com/zoopark/rv/animation/FlipItemAnimator.java
@@ -1,6 +1,7 @@
 package com.zoopark.rv.animation;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewPropertyAnimator;
 
@@ -15,7 +16,7 @@ public class FlipItemAnimator extends BaseItemAnimator {
     }
 
     @Override
-    protected void addAnimationInit(RecyclerView.ViewHolder holder) {
+    protected boolean addAnimationInit(RecyclerView.ViewHolder holder) {
         switch (mMark) {
             case X:
                 holder.itemView.setRotationX(-90);
@@ -25,19 +26,20 @@ public class FlipItemAnimator extends BaseItemAnimator {
                 holder.itemView.setAlpha(0f);
                 break;
         }
+        return true;
     }
 
     @Override
-    protected ViewPropertyAnimator addAnimation(ViewPropertyAnimator animation) {
+    protected ViewPropertyAnimator addAnimation(ViewPropertyAnimator animator) {
         switch (mMark) {
             case X:
-                animation.rotationX(0);
+                animator.rotationX(0);
                 break;
             case Y:
-                animation.rotationY(0).alpha(1f);
+                animator.rotationY(0).alpha(1f);
                 break;
         }
-        return animation;
+        return animator;
     }
 
     @Override
@@ -48,15 +50,65 @@ public class FlipItemAnimator extends BaseItemAnimator {
     }
 
     @Override
-    protected ViewPropertyAnimator removeAnimation(ViewPropertyAnimator animation) {
+    protected ViewPropertyAnimator removeAnimation(ViewPropertyAnimator animator) {
         switch (mMark) {
             case X:
-                animation.rotationX(-90);
+                animator.rotationX(-90);
                 break;
             case Y:
-                animation.rotationY(-90).alpha(0f);
+                animator.rotationY(-90).alpha(0f);
                 break;
         }
-        return animation;
+        return animator;
+    }
+
+    @Override
+    protected boolean changeExitAnimationInit(RecyclerView.ViewHolder holder) {
+        holder.itemView.setRotationX(0);
+        holder.itemView.setRotationY(0);
+        holder.itemView.setAlpha(1f);
+        return true;
+    }
+
+    @Override
+    protected boolean changeEnterAnimationInit(RecyclerView.ViewHolder holder) {
+        holder.itemView.setAlpha(0f);
+        switch (mMark) {
+            case X:
+                holder.itemView.setRotationX(-180);
+                break;
+            case Y:
+                holder.itemView.setRotationY(-180);
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    protected ViewPropertyAnimator changeExitAnimation(ViewPropertyAnimator animator) {
+        animator.alpha(0f);
+        switch (mMark) {
+            case X:
+                animator.rotationX(-180);
+                break;
+            case Y:
+                animator.rotationY(-180);
+                break;
+        }
+        return animator;
+    }
+
+    @Override
+    protected ViewPropertyAnimator changeEnterAnimation(ViewPropertyAnimator animator) {
+        animator.alpha(1f);
+        switch (mMark) {
+            case X:
+                animator.rotationX(-360);
+                break;
+            case Y:
+                animator.rotationY(-360);
+                break;
+        }
+        return animator;
     }
 }
